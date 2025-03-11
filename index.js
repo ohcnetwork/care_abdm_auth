52eaaad8c49a607b1b57094109d397c10ff579b1
Khavin Shankar <khavinshankar@gmail.com>
2025-03-11 11:37:32 +0530

    added caching support

--- index.js
+++ index.js
@@ -7,32 +7,44 @@ const {
   ABDM_CLIENT_SECRET,
 } = require("./config");
 
+let cache = null;
+let cacheTimestamp = null;
+let cacheExpiry = null;
+
 const server = http.createServer(async (req, res) => {
   res.setHeader("Content-Type", "application/json");
 
   if (req.url === "/" && req.method === "POST") {
     try {
-      const response = await fetch(ABDM_AUTH_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          clientId: ABDM_CLIENT_ID,
-          clientSecret: ABDM_CLIENT_SECRET,
-        }),
-      });
+      if (!cache || Date.now() - cacheTimestamp > cacheExpiry) {
+        const response = await fetch(ABDM_AUTH_URL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            clientId: ABDM_CLIENT_ID,
+            clientSecret: ABDM_CLIENT_SECRET,
+          }),
+        });
 
-      if (!response.ok) {
-        res.statusCode = response.status;
+        if (!response.ok) {
+          res.statusCode = response.status;
 
-        res.end(JSON.stringify({ error: "Failed to retrieve data" }));
-        return;
+          res.end(JSON.stringify({ error: "Failed to retrieve data" }));
+          return;
+        }
+
+        const data = await response.json();
+
+        cache = data;
+        cacheTimestamp = Date.now();
+        cacheExpiry = (data.expiresIn ?? 1200) * 1000;
+        console.log("Cache Updated");
       }
 
-      const data = await response.json();
       res.statusCode = 200;
-      res.end(JSON.stringify(data));
+      res.end(JSON.stringify(cache));
     } catch (error) {
       console.error("Error calling ABDM Gateway service:", error);
       res.statusCode = 500;
